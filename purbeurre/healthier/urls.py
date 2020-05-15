f5d5c96b515af1a4d8b961b1119a17ec546229ab
Paul <paulgirmes@hotmail.com>
2020-05-15 19:13:14 +0200

    style+pages creation

--- purbeurre/healthier/urls.py
+++ purbeurre/healthier/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path("resultats", views.results, name='results'),
     path("contact", views.contact, name="contact"),
     path("mentions_legales", views.general_conditions, name="general_conditions"),
+    path("food_item", views.fooditem, name="fooditem")
 ]
--- purbeurre/healthier/views.py
+++ purbeurre/healthier/views.py
@@ -12,20 +12,32 @@ def myaccount(request):
     return HttpResponse('myaccount')
 
 def myfoods(request):
-    return HttpResponse('myfoods')
+    message ={"food_items":["item1", "item2", "item3","item1", "item2", "item3","item1", "item2", "item3","item1", "item2", "item3","item1", "item2", "item3","item1", "item2", "item3","item1", "item2", "item3","item1", "item2", "item3","item1", "item2", "item3","item1", "item2", "item3","item1", "item2", "item3"]}
+    return render(request, "healthier/_my_saved_foods.html", message)
 
 def logout(request):
     return HttpResponse('logout')
 
 def results(request):
-    obj = str(request.GET)
     query = request.GET['fooditem']
-    message = "propriété GET : {} et requête : {}".format(obj, query)
-    return HttpResponse(message)
+    message = {"food_items":[{"id":"1"}, {"id":"1"}, {"id":"1"},{"id":"1"}, {"id":"1"}, {"id":"1"}, {"id":"1"}, {"id":"1"},],
+                "img_url":"https://static.openfoodfacts.org/images/products/316/544/000/8935/front_fr.18.full.jpg",
+                "searched": query
+                }
+    return render(request, "healthier/_results.html", message)
 
 def contact(request):
-    return HttpResponse("contacts")
+    return render(request, "healthier/_contact.html")
 
+def fooditem(request):
+    message = {
+        "name": "pates",
+        "nutriscore": "a",
+        "energy_kcal_100gr":"250",
+        "img_url":"https://static.openfoodfacts.org/images/products/316/544/000/8935/front_fr.18.full.jpg",
+        "openfoodfacts_url":"https://fr.openfoodfacts.org/produit/3165440008621/cereales-gourmandes-tipiak"
+    }
+    return render(request, "healthier/_food_item.html", message)
 
 def general_conditions(resquest):
     return HttpResponse("legal")
