1ec52ad036058a1bfa61e18c748be4d13f46cac2
Paul <paulgirmes@hotmail.com>
2020-05-21 18:28:01 +0200

    added command to populate DB

--- purbeurre/healthier/models.py
+++ purbeurre/healthier/models.py
@@ -8,11 +8,13 @@ class Store(models.Model):
     def __repr__(self):
         print(self.name)
 
+
 class Brand(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
     def __repr__(self):
         print(self.name)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200, unique=True)
@@ -22,19 +24,19 @@ class Category(models.Model):
 
 
 class Food_item(models.Model):
-    open_food_facts_url = models.URLField(max_length=400, unique=True)
-    name = models.CharField(max_length=200, unique=True)
-    energy_kcal_100gr = models.BigIntegerField()
-    nutri_score_fr = models.CharField(max_length=1, choices=[("a", "a"), ("b","b"), ("c","c"), ("d","d")])
-    nova_grade = models.IntegerField(choices=[(1,1), (2,2), (3,3),(4,4)])
-    image_url = models.URLField(max_length=400, unique=True)
+    open_food_facts_url = models.URLField(max_length=400)
+    name = models.CharField(max_length=200)
+    energy_100g = models.CharField(max_length=20)
+    nutri_score_fr = models.CharField(
+        max_length=1, choices=[("a", "a"), ("b", "b"), ("c", "c"), ("d", "d")]
+    )
+    nova_grade = models.IntegerField(choices=[(1, 1), (2, 2), (3, 3), (4, 4)])
+    image_url = models.URLField(max_length=400)
     id_open_food_facts = models.BigIntegerField(unique=True)
     stores = models.ManyToManyField(Store)
-    Brands = models.ManyToManyField(Brand)
+    brands = models.ManyToManyField(Brand)
     categories = models.ManyToManyField(Category)
     favoris = models.ManyToManyField(User)
 
-
     def __repr__(self):
         print(self.name + " / " + self.open_food_facts_url)
-
