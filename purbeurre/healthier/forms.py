627654f8ebe257008b10d18909585bbec99ecdbc
Paul <paulgirmes@hotmail.com>
2020-05-25 11:07:43 +0200

    added login+auth

--- purbeurre/healthier/forms.py
+++ purbeurre/healthier/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
+from django.contrib.auth.models import User
+from django.contrib.auth import  password_validation, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
+from django.db.utils import IntegrityError
+from django.forms import ModelForm, ValidationError
+
 
 class FoodQuery(forms.Form):
 
@@ -18,4 +24,66 @@ class FoodQuery(forms.Form):
         self.helper.form_method = 'get'
         self.helper.form_action = "healthier:results"
         self.helper.add_input(Submit(self.auto_id, 'chercher'))
+
+class Signin(UserCreationForm):
+    
+
+    first_name = forms.CharField(required=True, label="prénom",widget=forms.TextInput())
+    email = forms.EmailField(required=True, label="email")
+
+    def __init__(self, request=None,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_method = 'POST'
+        self.helper.form_action = "healthier:login"
+        self.helper.add_input(Submit("Signin", "Créer Mon Compte"))
+        self.user = None
+        self.request = request
+
+    class Meta:
+        model = User
+        fields = ["first_name", "email"]
+    
+    def save(self, commit=True):
+        try:
+            self.is_valid()
+            self.clean_password2()
+            user = super().save(commit=False)
+        except:
+            return self.errors
+        user.username = user.email
+        user.set_password(self.cleaned_data["password1"])
+        try:
+            if commit:
+                user.save()
+                self.user = user
+                login(self.request, self.user)
+                return True
+        except IntegrityError:
+            self.add_error('email', self.data["email"]+" est déjà utilisé par un autre compte, merci d'en utiliser un autre")
+            return self.errors
+        
+    
+
+
+
+class Login(AuthenticationForm):
+
+    username = UsernameField(label="email",widget=forms.TextInput(attrs={'autofocus': True}))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_method = 'GET'
+        self.helper.form_action = "healthier:login"
+        self.helper.add_input(Submit("Login", "Se connecter"))
+
+
+    def log_user(self):
+        try:
+            self.is_valid()
+            self.clean()
+            login(self.request, self.get_user())
+        except ValidationError as e:
+            raise ValidationError("E-mail et/ou mot de passe invalides " +"("+str(e)+")")
 
