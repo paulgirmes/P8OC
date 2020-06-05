ead02a7a8620e3ccad624eb9532c85568d75febc
Paul <paulgirmes@hotmail.com>
2020-06-05 20:13:00 +0200

    added views_tests

--- purbeurre/healthier/views.py
+++ purbeurre/healthier/views.py
@@ -1,6 +1,12 @@
 import json
-from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseServerError, Http404, HttpResponseServerError, HttpResponseForbidden
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import (
+    HttpResponse,
+    HttpResponseServerError,
+    Http404,
+    HttpResponseServerError,
+    HttpResponseForbidden,
+)
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
@@ -17,39 +23,43 @@ def home(request):
     request.session.set_expiry(0)
     form = FoodQuery(auto_id="form")
     form1 = FoodQuery(auto_id="form1")
-    context={'form': form, 'form1': form1}
+    context = {"form": form, "form1": form1}
     return render(request, "healthier/_index.html", context)
+
 
 @login_required(login_url="healthier:login")
 def myaccount(request):
     form1 = FoodQuery(auto_id="form1")
     context = {
-        'form1': form1,
+        "form1": form1,
         "user_name": request.user.first_name,
         "user_mail": request.user.email,
     }
     return render(request, "healthier/_user_page.html", context)
 
+
 @login_required(login_url="healthier:login")
 def myfoods(request):
     form1 = FoodQuery(auto_id="form1")
-    message ={
-        'form1': form1,
+    message = {
+        "form1": form1,
         "food_items": "",
-        }
+    }
     favorites = Food_item.get_favorites(request.user.username)
-    message["food_items"]=favorites
+    message["food_items"] = favorites
     return render(request, "healthier/_my_saved_foods.html", message)
 
 
 def login(request):
-    request.session["visited"]=True
+    request.session["visited"] = True
     form1 = FoodQuery(auto_id="form1")
     if not request.user.is_authenticated:
         sign_form = Signin(auto_id="signin%s")
         log_form = Login(auto_id="login%s")
-        message ={
-        'form1': form1, 'sign_form': sign_form, "log_form": log_form,
+        message = {
+            "form1": form1,
+            "sign_form": sign_form,
+            "log_form": log_form,
         }
         if request.method == "POST":
             sign_form = Signin(request=request, data=request.POST, auto_id="signin%s")
@@ -58,7 +68,9 @@ def login(request):
                 if e == True:
                     return redirect(reverse("healthier:myaccount"))
                 else:
-                    message.update({"sign_form":sign_form, "modaltoshow":"SigninModal"})
+                    message.update(
+                        {"sign_form": sign_form, "modaltoshow": "SigninModal"}
+                    )
             except:
                 return HttpResponseServerError
         elif "username" in request.GET:
@@ -67,7 +79,7 @@ def login(request):
                 log_form.log_user()
                 return redirect(reverse("healthier:myaccount"))
             except:
-                message.update({"modaltoshow":"LoginModal", "log_form":log_form})
+                message.update({"modaltoshow": "LoginModal", "log_form": log_form})
 
         return render(request, "healthier/_login_signin.html", message)
     else:
@@ -75,46 +87,51 @@ def login(request):
         return redirect(reverse("healthier:home"))
     return render(request, "healthier/_results.html", message)
 
+
 def contact(request):
-    request.session["visited"]=True
-    form1=FoodQuery(auto_id="form1")
+    request.session["visited"] = True
+    form1 = FoodQuery(auto_id="form1")
     message = {
-        'form1': form1,
-        }
+        "form1": form1,
+    }
     return render(request, "healthier/_contact.html", message)
 
+
 def fooditem(request):
-    request.session["visited"]=True
+    request.session["visited"] = True
     form1 = FoodQuery(auto_id="form1")
     message = {
-        "form1":form1,
-        "food_item":"",
+        "form1": form1,
+        "food_item": "",
     }
-    message["food_item"]= Food_item.objects.get(id=request.GET["food_id"])
+    message["food_item"] = get_object_or_404(Food_item.objects, id=request.GET["food_id"])
     return render(request, "healthier/_food_item.html", message)
 
+
 def general_conditions(request):
-    request.session["visited"]=True
+    request.session["visited"] = True
     form1 = FoodQuery(auto_id="form1")
     message = {
-        'form1': form1,
-        }
+        "form1": form1,
+    }
     return render(request, "healthier/_legal_content.html", message)
 
+
 def results(request):
-    request.session["visited"]=True
+    request.session["visited"] = True
     form1 = FoodQuery(auto_id="form1")
     message = {
-        'form1': form1,
-        "food_items":"",
+        "form1": form1,
+        "food_items": "",
         "searched": "",
-            }
+    }
     if "form" in request.GET or "form1" in request.GET:
         form = FoodQuery(data=request.GET, auto_id="form")
-        if form.is_valid() :
-            results = Food_item.get_searched_food_Item(food_name=form.cleaned_data.get("name"))          
+        if form.is_valid():
+            results = Food_item.get_searched_food_Item(
+                food_name=form.cleaned_data.get("name")
+            )
         else:
-            form.add_error(None,"Champ Invalide")
             message.update({"form": form})
             return render(request, "healthier/_no_results.html", message)
     elif "id" in request.GET:
@@ -122,30 +139,49 @@ def results(request):
     elif request.method == "POST":
         result = Food_item.save_favorites(request.POST["value"], request.user)
         return HttpResponse(json.dumps(result))
-    
-    if  results["status"] == "ok":
-        message["food_items"]=results["replacement_items"]
-        message["searched"]=results["to_be_replaced_item"]
+    else:
+        raise Http404("pas d'aliments recherchés dans la requette")
+
+    if results["status"] == "ok":
+        message["food_items"] = results["replacement_items"]
+        message["searched"] = results["to_be_replaced_item"]
         return render(request, "healthier/_results.html", message)
 
     elif results["status"] == "choice_to_make":
         if results["to_be_replaced_item"].count() < 100:
-            form.add_error(None, "Il existe " + str(results["to_be_replaced_item"].count())+
-                            " aliments contenant '"+form.cleaned_data.get("name")+"' !"
-                            " merci de choisir l'aliment à remplacer dans la liste ci dessous."
-                            )
+            form.add_error(
+                None,
+                "Il existe "
+                + str(results["to_be_replaced_item"].count())
+                + " aliments contenant '"
+                + form.cleaned_data.get("name")
+                + "' !"
+                " merci de choisir l'aliment à remplacer dans la liste ci dessous.",
+            )
         else:
-            form.add_error(None, "Il existe " + str(results["to_be_replaced_item"].count())+
-                            " aliments contenant '"+form.cleaned_data.get("name")+"' !"
-                            " merci de préciser votre recherche."
-                            )
+            form.add_error(
+                None,
+                "Il existe "
+                + str(results["to_be_replaced_item"].count())
+                + " aliments contenant '"
+                + form.cleaned_data.get("name")
+                + "' !"
+                " merci de préciser votre recherche.",
+            )
         message.update({"form": form})
-        message["searched"]=results["to_be_replaced_item"]        
-        
+        message["searched"] = results["to_be_replaced_item"]
+
     elif results["status"] == "not_found":
-        form.add_error(None, form.cleaned_data.get("name") + " est introuvable dans notre liste d'aliments ! Merci de renouveller votre recherche")
+        form.add_error(
+            None,
+            form.cleaned_data.get("name")
+            + " est introuvable dans notre liste d'aliments ! Merci de renouveller votre recherche",
+        )
         message.update({"form": form})
     elif results["status"] == "no_replacement":
-        form.add_error(None, "il n'existe pas à ce jour d'aliment de remplacement plus sain dans notre base de données.")
+        form.add_error(
+            None,
+            "il n'existe pas à ce jour d'aliment de remplacement plus sain dans notre base de données.",
+        )
         message.update({"form": form})
     return render(request, "healthier/_no_results.html", message)
---
+++ purbeurre/tests/unit/healthier/test_views.py
@@ -0,0 +1,452 @@
+from unittest import mock
+from django.test import TestCase
+from django.urls import reverse
+from django.http import Http404
+from django.contrib.auth.models import User
+from healthier.models import Food_item, Category
+
+
+def setup():
+    testuser = User.objects.create_user(
+        "google@google.com",
+        email="google@google.com",
+        password="123456789",
+        first_name="Joe",
+    )
+    food = Food_item.objects.create(
+        open_food_facts_url="https://fr.openfoodfacts.org/produit/3103220009512/chamallows-haribo",
+        name="Chamallows",
+        nutri_score_fr="d",
+        nova_grade=3,
+        image_url="https://static.openfoodfacts.org/images/products/310/322/000/9512/front_fr.54.400.jpg",
+        id_open_food_facts="1",
+        energy_100g="326kcal",
+        image_nutrition_url="326kcal",
+    )
+    food2 = Food_item.objects.create(
+        open_food_facts_url="https://fr.openfoodfacts.org/produit/3103220009512/chamallows-haribo",
+        name="Chamallows mallows",
+        nutri_score_fr="a",
+        nova_grade=2,
+        image_url="https://static.openfoodfacts.org/images/products/310/322/000/9512/front_fr.54.400.jpg",
+        id_open_food_facts="2",
+        energy_100g="326kcal",
+        image_nutrition_url="326kcal",
+    )
+    cat = Category.objects.create(name="bonbons")
+    food.categories.add(cat)
+    food2.categories.add(cat)
+    food.favoris.add(testuser)
+
+
+class Test_purbeurre_urls(TestCase):
+    def test_admin_exists(self):
+        response = self.client.get("/admin/")
+        self.assertRedirects(response, "/admin/login/?next=%2Fadmin%2F")
+
+    def test_admin_url_accessible_by_name(self):
+        response = self.client.get(reverse("admin:login"))
+        self.assertEqual(response.status_code, 200)
+
+
+class Test_Purbeurre_healthier_home(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        setup()
+
+    def test_home_exists(self):
+        response = self.client.get("")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_url_accessible_by_name(self):
+        response = self.client.get(reverse("healthier:home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_uses_correct_template(self):
+        response = self.client.get(reverse("healthier:home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "healthier/_index.html")
+
+    def test_home_data_privacy_modal_not_logged_in(self):
+        response = self.client.get(reverse("healthier:home"))
+        self.assertContains(response, "modal fade")
+        # check that modal is not in response when session cookie is set by an other view
+        self.client.get(reverse("healthier:contact"))
+        response = self.client.get(reverse("healthier:home"))
+        self.assertNotContains(response, "modal fade")
+
+    def test_home_data_privacy_modal_logged_in(self):
+        self.assertTrue(
+            self.client.login(username="google@google.com", password="123456789")
+        )
+        response = self.client.get(reverse("healthier:home"))
+        self.assertNotContains(response, "modal fade")
+
+    def test_home_nav_header_not_logged_in(self):
+        response = self.client.get(reverse("healthier:home"))
+        self.assertContains(response, 'title="Loggin / Créer un compte"')
+        self.assertNotContains(response, 'title="Mon Compte"')
+
+    def test_home_nav_header_logged_in(self):
+        self.assertTrue(
+            self.client.login(username="google@google.com", password="123456789")
+        )
+        response = self.client.get(reverse("healthier:home"))
+        self.assertNotContains(response, 'title="Loggin / Créer un compte"')
+        self.assertContains(response, 'title="Mon Compte"')
+
+    def test_home_forms_exists(self):
+        response = self.client.get(reverse("healthier:home"))
+        self.assertTrue("form" in response.context)
+        self.assertTrue("form1" in response.context)
+
+
+class Test_Purbeurre_healthier_myaccount(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        setup()
+
+    def test_myaccount_exists_when_logged_in(self):
+        self.assertTrue(
+            self.client.login(username="google@google.com", password="123456789")
+        )
+        response = self.client.get("/moncompte")
+        self.assertEqual(response.status_code, 200)
+
+    def test_myaccount_url_accessible_by_name_when_logged_in(self):
+        self.assertTrue(
+            self.client.login(username="google@google.com", password="123456789")
+        )
+        response = self.client.get(reverse("healthier:myaccount"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_myaccount_uses_correct_template_when_logged_in(self):
+        self.assertTrue(
+            self.client.login(username="google@google.com", password="123456789")
+        )
+        response = self.client.get(reverse("healthier:myaccount"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "healthier/_user_page.html")
+
+    def test_myaccount_returns_context_when_logged_in(self):
+        self.assertTrue(
+            self.client.login(username="google@google.com", password="123456789")
+        )
+        response = self.client.get(reverse("healthier:myaccount"))
+        self.assertTrue("form" in response.context)
+        self.assertTrue("user_name" in response.context)
+        self.assertTrue("Joe" == response.context["user_name"])
+        self.assertTrue("user_mail" in response.context)
+        self.assertTrue("google@google.com" == response.context["user_mail"])
+
+    def test_myaccount_redirects_when_not_logged_in(self):
+        response = self.client.get(reverse("healthier:myaccount"))
+        self.assertRedirects(
+            response, reverse("healthier:login") + "?next=%2Fmoncompte"
+        )
+
+
+class Test_Purbeurre_healthier_myfoods(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        setup()
+
+    def test_myfoods_exists_when_logged_in(self):
+        self.assertTrue(
+            self.client.login(username="google@google.com", password="123456789")
+        )
+        response = self.client.get("/mesaliments")
+        self.assertEqual(response.status_code, 200)
+
+    def test_myfoods_url_accessible_by_name_when_logged_in(self):
+        self.assertTrue(
+            self.client.login(username="google@google.com", password="123456789")
+        )
+        response = self.client.get(reverse("healthier:myfoods"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_myfoods_uses_correct_template_when_logged_in(self):
+        self.assertTrue(
+            self.client.login(username="google@google.com", password="123456789")
+        )
+        response = self.client.get(reverse("healthier:myfoods"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "healthier/_my_saved_foods.html")
+
+    def test_myfoods_redirects_when_not_logged_in(self):
+        response = self.client.get(reverse("healthier:myfoods"))
+        self.assertRedirects(
+            response, reverse("healthier:login") + "?next=%2Fmesaliments"
+        )
+
+    def test_my_food_response_has_content_when_logged_in(self):
+        self.assertTrue(
+            self.client.login(username="google@google.com", password="123456789")
+        )
+        response = self.client.get(reverse("healthier:myfoods"))
+        self.assertTrue("form1" in response.context)
+        self.assertTrue("food_items" in response.context)
+        self.assertContains(response, "Chamallows")
+
+
+class Test_Purbeurre_healthier_results(TestCase):
+    
+
+    @classmethod
+    def setUpTestData(cls):
+        setup()
+
+    def test_results_url_exists_when_data(self):
+        response = self.client.get("/resultats", data={"id": 9})
+        self.assertEqual(response.status_code, 200)
+
+    def test_results_url_accessible_by_name_when_data(self):
+        response = self.client.get(reverse("healthier:results"), data={"id": 9})
+        self.assertEqual(response.status_code, 200)
+
+    def test_results_uses_correct_template_when_no_results(self):
+        response = self.client.get(reverse("healthier:results"), data={"form": 18})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "healthier/_no_results.html")
+
+    def test_results_uses_correct_template_when_result(self):
+        response = self.client.get(reverse("healthier:results"), data={"id": 9})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "healthier/_results.html")
+
+    def test_results_raises_404_when_no_data(self):
+        response = self.client.get(reverse("healthier:results"))
+        self.assertEqual(response.status_code, 404)
+
+    def test_results_forms_invalid_field_error(self):
+        data = 201 * "x"
+        response = self.client.get(
+            reverse("healthier:results"), data={"form": "form", "name": data}
+        )
+        self.assertEqual(
+            response.context["form"].errors,
+            {
+                "name": [
+                    "Assurez-vous que cette valeur comporte au plus 200 caractères (actuellement 201)."
+                ]
+            },
+        )
+        response = self.client.get(
+            reverse("healthier:results"), data={"form1": "form", "name": data}
+        )
+        self.assertEqual(
+            response.context["form"].errors,
+            {
+                "name": [
+                    "Assurez-vous que cette valeur comporte au plus 200 caractères (actuellement 201)."
+                ]
+            },
+        )
+
+    def test_results_returns_result_when_POST(self):
+        
+        def mock_Food_item_save_favorites(self, *args, **kwargs):
+            return {"result": "added", "status": True}
+        
+        with mock.patch(
+            "healthier.views.Food_item.save_favorites",
+            new=mock_Food_item_save_favorites
+        ):
+            response = self.client.post(
+                reverse("healthier:results"),
+                data={"value": "XXXXXX"}
+            )
+            self.assertContains(response, "added")
+
+    def test_results_when_form_is_valid(self):
+
+        # workaround to mock <queryset>.count() method
+        # used on results["to_be_replaced_items"] in views.results :
+        class Queryset(list):
+            def __init__(self, number=0):
+                self.number = number
+            def count(self):
+                return self.number
+
+        def mock_Food_item_get_searched_food_Item(food_id=None, food_name=None):
+            if food_name == "OK":
+                return {"status":"ok", "replacement_items": "several", "to_be_replaced_item" : "one"}
+            if food_name == "choice_to_make":
+                return {"status":"choice_to_make", "replacement_items": "none", "to_be_replaced_item" : Queryset(99)}
+            if food_name == "choice_to_make>100":
+                return {"status":"choice_to_make", "replacement_items": "several", "to_be_replaced_item" : Queryset(101)}
+            if food_name == "not_found":
+                return {"status":"not_found", "replacement_items": "none", "to_be_replaced_item" : "one"}
+            if food_name == "no_replacement":
+                return {"status":"no_replacement", "replacement_items": "none", "to_be_replaced_item" : "one"}
+
+        with mock.patch(
+            "healthier.views.Food_item.get_searched_food_Item",
+            new=mock_Food_item_get_searched_food_Item
+        ):
+            # first case : form is valid and 
+            # query returns several items from fooditem name provided items number <100:
+            response = self.client.get(
+                reverse("healthier:results"),
+                data={"form": "form", "name": "choice_to_make"},
+            )
+            self.assertEqual(
+                response.context["form"].errors,
+                {
+                    "__all__": [
+                        "Il existe 99"
+                        " aliments contenant 'choice_to_make' !"
+                        " merci de choisir l'aliment à remplacer dans la liste ci dessous."
+                    ]
+                },
+            )
+            # second case one item to be replaced is found and 
+            # several replacement items are returned:
+            response = self.client.get(
+                reverse("healthier:results"),
+                data={"form": "form", "name": "OK"},
+            )
+            self.assertEqual(
+                response.context["food_items"],
+                "several"
+            )
+            self.assertEqual(
+                response.context["searched"],
+                "one"
+            )
+            # third case query returns several items from fooditem name provided 
+            # items number >100:
+            response = self.client.get(
+                reverse("healthier:results"),
+                data={"form": "form", "name": "choice_to_make>100"},
+            )
+            self.assertEqual(
+                response.context["form"].errors,
+                {
+                    "__all__": [
+                        "Il existe 101"
+                        " aliments contenant 'choice_to_make>100' !"
+                        " merci de préciser votre recherche."
+                    ]
+                },
+            )
+            # fourth case query does not find a food item from fooditem name provided:
+            response = self.client.get(
+                reverse("healthier:results"),
+                data={"form": "form", "name": "not_found"},
+            )
+            self.assertEqual(
+                response.context["form"].errors,
+                {
+                    "__all__": [
+                        "not_found "
+                        "est introuvable dans notre liste d'aliments ! "
+                        "Merci de renouveller votre recherche"
+                    ]
+                },
+            )
+            # fith case query does not find any replacement items from the given food name:
+            response = self.client.get(
+                reverse("healthier:results"),
+                data={"form": "form", "name": "no_replacement"},
+            )
+            self.assertEqual(
+                response.context["form"].errors,
+                {
+                    "__all__": [
+                        "il n'existe pas à ce jour d'aliment de remplacement plus sain dans notre base de données."
+                    ]
+                },
+            )
+
+
+class Test_Purbeurre_healthier_contact(TestCase):
+
+    def test_contact_exists(self):
+        response = self.client.get("/contact")
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact_url_accessible_by_name(self):
+        response = self.client.get(reverse("healthier:contact"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact_uses_correct_template(self):
+        response = self.client.get(reverse("healthier:contact"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'healthier/_contact.html')
+    
+    def test_fomr1_in_response(self):
+        response = self.client.get(reverse("healthier:contact"))
+        self.assertContains(response, "form1")
+
+
+class Test_Purbeurre_healthier_general_conditions(TestCase):
+
+
+    def test_general_conditions_exists(self):
+        response = self.client.get("/mentions_legales")
+        self.assertEqual(response.status_code, 200)
+
+    def test_general_conditions_url_accessible_by_name(self):
+        response = self.client.get(reverse("healthier:general_conditions"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_general_uses_correct_template(self):
+            response = self.client.get(reverse("healthier:general_conditions"))
+            self.assertEqual(response.status_code, 200)
+            self.assertTemplateUsed(response, 'healthier/_legal_content.html')
+
+    def test_fomr1_in_response(self):
+        response = self.client.get(reverse("healthier:general_conditions"))
+        self.assertContains(response, "form1")
+
+
+class Test_Purbeurre_healthier_fooditem(TestCase):
+
+
+    @classmethod
+    def setUpTestData(cls):
+        setup()
+
+    def test_food_item_exists(self):
+        response = self.client.get("/food_item", data={"food_id":1})
+        self.assertEqual(response.status_code, 200)
+
+    def test_food_item_url_accessible_by_name(self):
+        response = self.client.get(reverse("healthier:fooditem"), data={"food_id":1})
+        self.assertEqual(response.status_code, 200)
+
+    def test_food_uses_correct_template(self):
+        response = self.client.get(reverse("healthier:fooditem"), data={"food_id":1})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'healthier/_food_item.html')
+    
+    def test_fomr1_in_response(self):
+        response = self.client.get(reverse("healthier:fooditem"), data={"food_id":1})
+        self.assertContains(response, "form1")
+
+    def test_food_item_is_returned_when_found(self):
+        response = self.client.get(reverse("healthier:fooditem"), data={"food_id":1})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["food_item"], Food_item.objects.get(id=1) )
+
+    def test_404_is_returned_when_not_found(self):
+        response = self.client.get(reverse("healthier:fooditem"), data={"food_id":18})
+        self.assertEqual(response.status_code, 404)
+
+
+# class Test_Purbeurre_healthier_login(TestCase):
+
+
+#     def test_login_exists(self):
+#         response = self.client.get("/login")
+#         self.assertEqual(response.status_code, 200)
+
+#     def test_login_url_accessible_by_name(self):
+#         response = self.client.get(reverse("healthier:login"))
+#         self.assertEqual(response.status_code, 200)
+
+#     def test_login_uses_correct_template(self):
+#             response = self.client.get(reverse("healthier:login"))
+#             self.assertEqual(response.status_code, 200)
+#             self.assertTemplateUsed(response, 'healthier/_login_signin.html')
