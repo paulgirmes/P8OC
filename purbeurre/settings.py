279897443867b4bcc49d830ea4f38a9f5292b3da
Paul <paulgirmes@hotmail.com>
2020-06-11 17:42:08 +0200

    -

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIR = {
-    os.path.join(BASE_DIR, "healthier/static")
+    os.path.join(BASE_DIR, "healthier/static/healthier")
 }
 
 
