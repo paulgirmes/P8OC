78759433252ad55330ea0c388f4f34b75f8f5740
Paul <paulgirmes@hotmail.com>
2020-06-11 18:00:21 +0200

    -debug

--- purbeurre/urls.py
+++ purbeurre/urls.py
@@ -16,13 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
-import healthier
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls, name="admin"),
-    path("", include(healthier.urls))
+    path("", include('healthier.urls' namespace="healthier"))
 ]
 
 
